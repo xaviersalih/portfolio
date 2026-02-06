210ab8dd49270ca93095d015e455486eaca4a807
xaviersalih <xaviersalih@gmail.com>
2026-02-06 16:40:28 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document
-      .querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
